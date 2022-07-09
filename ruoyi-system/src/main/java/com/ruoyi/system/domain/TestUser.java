c556863a93b60b306cb533fa1436d5069800bcee
山有木兮木有枝 <8227036+a602468057@user.noreply.gitee.com>
2022-07-10 00:47:26 +0800

    ''

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/TestUser.java
@@ -0,0 +1,5 @@
+package com.ruoyi.system.domain;
+
+public class TestUser {
+
+}
---
+++ ruoyi-ui/src/api/test/institute.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+//查询综测院系用户
+export function TestUser(query) {
+  return request({
+    url: '/test/institute',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询用户详细
+export function getTestUser(userId) {
+  return request({
+    url: '/test/institute/' + parseStrEmpty(userId),
+    method: 'get'
+  })
+}
