3147ffe54b1f785798b3e1af7d530cd3ab0816ed
山有木兮木有枝 <8227036+a602468057@user.noreply.gitee.com>
2022-08-12 19:00:32 +0800

    '院系管理和专业管理java'

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/test/TestUserMajorController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/test/TestUserMajorController.java
@@ -5,18 +5,21 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.test.domain.Student;
 import com.ruoyi.test.domain.TestUser;
+import com.ruoyi.test.domain.Testersoure;
+import com.ruoyi.test.service.IStudentService;
 import com.ruoyi.test.service.ITestUserService;
+import com.ruoyi.test.service.ITestersoureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * testController
- * 
+ *
  * @author nfca
  * @date 2022-07-12
  */
@@ -27,37 +30,78 @@ public class TestUserMajorController extends BaseController
     @Autowired
     private ITestUserService testUserService;
 
+
+    @Autowired
+    private IStudentService studentService;
+
+    @Autowired
+    private ITestersoureService testersoureService;
     /**
      * 查询test列表
      */
-    @PreAuthorize("@ss.hasPermi('test:major:list')")
-//    @PreAuthorize("@ss.hasRole('instructor')")
+/*    @PreAuthorize("@ss.hasPermi('test:class:list')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @GetMapping("/list")
     public TableDataInfo list(TestUser testUser)
     {
         startPage();
         List<TestUser> list = testUserService.selectTestUserList(testUser);
         return getDataTable(list);
+    }*/
+
+    /**
+     * 查询学生列表
+     */
+    @PreAuthorize("@ss.hasPermi('test:major:list')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
+    @GetMapping("/list")
+    public TableDataInfo list(Student student)
+    {
+        startPage();
+        List<Student> list = studentService.selectAllStudents(student);
+        return getDataTable(list);
     }
+
+    /*
+     *  ID查询学生
+     * */
+    /*@PreAuthorize("@ss.hasRole('boss')")
+    @GetMapping(value = "/{no}")
+    public AjaxResult getInfo(@PathVariable("no") Integer no)
+    {
+        return AjaxResult.success(studentMapper.selectStudentByUserId(no));
+    }*/
 
 
 
     /**
      * 获取test详细信息
      */
-    @PreAuthorize("@ss.hasPermi('test:major:query')")
-//    @PreAuthorize("@ss.hasRole('instructor','boss')")
+    /*@PreAuthorize("@ss.hasPermi('test:class:query')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @GetMapping(value = "/{userId}")
     public AjaxResult getInfo(@PathVariable("userId") Long userId)
     {
         return AjaxResult.success(testUserService.selectTestUserByUserId(userId));
+    }*/
+
+    @PreAuthorize("@ss.hasPermi('test:major:query')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
+    @GetMapping(value = "/{no}")
+    public AjaxResult getInfo(@PathVariable("no") Integer no)
+    {
+        return AjaxResult.success(studentService.selectTestUserByUserId(no));
     }
+
+
+
+
 
     /**
      * 修改test
      */
     @PreAuthorize("@ss.hasPermi('test:major:edit')")
-//    @PreAuthorize("@ss.hasRole('instructor','boss')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @Log(title = "test", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody TestUser testUser)
@@ -69,44 +113,55 @@ public class TestUserMajorController extends BaseController
      * 删除test
      */
     @PreAuthorize("@ss.hasPermi('test:major:remove')")
-//    @PreAuthorize("@ss.hasRole('instructor','boss')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @Log(title = "test", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{userIds}")
+    @DeleteMapping("/{userIds}")
     public AjaxResult remove(@PathVariable Long[] userIds)
     {
         return toAjax(testUserService.deleteTestUserByUserIds(userIds));
     }
 
 
-
-    @RequestMapping(value = "/detail/{userId}")
-    public String detailTest(@PathVariable("userId") Long userId, Model model)
+    /**
+     * 驳回学生申请
+     */
+    @PreAuthorize("@ss.hasPermi('test:major:back')")
+    @Log(title = "back", businessType = BusinessType.UPDATE)
+    @PutMapping("backUser")
+    /*public AjaxResult backTestUser(@RequestBody TestUser testUser)
     {
-        System.out.println("hhhhhhhhhhhhhhhhhh  " + userId + "  nnnnnn");
-        System.out.println("nimade");
-        return "redirect:test/class";
-
+        return toAjax(testUserService.backTestUser(testUser));
+    }*/
+    public AjaxResult backTestUser(@RequestBody Testersoure testersoure){
+        Student student = new Student();
+        student.setStatus("2");
+        student.setNo(testersoure.getNo());
+        studentService.updateStatusByNo(student);
+        return toAjax(testersoureService.updateReasonByNo(testersoure));
     }
 
+    /**
+     * 批量通过学生申请
+     */
+    @PreAuthorize("@ss.hasPermi('test:major:access')")
+    @Log(title = "access", businessType = BusinessType.UPDATE)
+    @PutMapping("/{nos}")
+    public AjaxResult accessUser(@PathVariable String nos){
+        return toAjax(studentService.updateStatusByNos(nos));
+    }
+
+    /**
+     * 通过单个学生
+     */
+    @PreAuthorize("@ss.hasPermi('test:major:access2')")
+    @Log(title = "access2", businessType = BusinessType.UPDATE)
+    @PutMapping("accessUser2")
+    public AjaxResult accessUser2(@RequestBody Student student){
+        student.setStatus("1");
+        return toAjax(studentService.updateStatusByNo(student));
+    }
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- ruoyi-ui/src/api/test/major.js
+++ ruoyi-ui/src/api/test/major.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 import { parseStrEmpty } from "@/utils/ruoyi";
 
-//查询综测专业用户列表
+//查询综测班级用户列表
 export function TestUser(query) {
   return request({
     url: '/test/major/list',
@@ -18,10 +18,60 @@ export function getTestUser(userId) {
   })
 }
 
-// 用户详情页跳转
-export function toDetail(userId) {
+
+/**
+ * 驳回学生申请
+ * @param no 学生学号
+ * @param reason 驳回原因
+ * @returns {*}
+ */
+export function backUser(no,reason) {
+  const data = {
+    no,
+    reason,
+    // userId,
+    // status
+    //infoIdList:infoId.join(','),
+    //status:status
+  }
   return request({
-    url: '/test/major/detail/' + userId,
-    method: 'get'
+    url: '/test/major/backUser',
+    method: 'put',
+    data: data
   })
 }
+
+/**
+ * 批量通过学生申请
+ * nos 为所选学生no数组
+ * @param nos
+ * @returns {*}
+ */
+export function accessUser(nos) {
+  const data = {
+    nos,
+  }
+  return request({
+    url: '/test/major/' + parseStrEmpty(nos),
+    method: 'put',
+    //data: data
+  })
+}
+
+
+/**
+ * 通过单个学生
+ * @param no 学生学号
+ * @returns {*}
+ */
+export function accessUser2(no) {
+  const data = {
+    no,
+  }
+  return request({
+    url: '/test/major/accessUser2',
+    method: 'put',
+    data: data
+  })
+}
+
