c8f9bccf062f298f2ea211a97e9be82dbbfdc3a4
我本无意穿堂风 <72974554+wh1t3zer@users.noreply.github.com>
2022-08-12 19:04:40 +0800

    Update TestUserInstituteController.java

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/test/TestUserInstituteController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/test/TestUserInstituteController.java
@@ -5,8 +5,12 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.test.domain.Student;
 import com.ruoyi.test.domain.TestUser;
+import com.ruoyi.test.domain.Testersoure;
+import com.ruoyi.test.service.IStudentService;
 import com.ruoyi.test.service.ITestUserService;
+import com.ruoyi.test.service.ITestersoureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +19,7 @@ import java.util.List;
 
 /**
  * testController
- * 
+ *
  * @author nfca
  * @date 2022-07-12
  */
@@ -26,37 +30,78 @@ public class TestUserInstituteController extends BaseController
     @Autowired
     private ITestUserService testUserService;
 
+
+    @Autowired
+    private IStudentService studentService;
+
+    @Autowired
+    private ITestersoureService testersoureService;
     /**
      * 查询test列表
      */
-//    @PreAuthorize("@ss.hasPermi('test:test:list')")
-    @PreAuthorize("@ss.hasRole('boss')")
+/*    @PreAuthorize("@ss.hasPermi('test:class:list')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @GetMapping("/list")
     public TableDataInfo list(TestUser testUser)
     {
         startPage();
         List<TestUser> list = testUserService.selectTestUserList(testUser);
         return getDataTable(list);
+    }*/
+
+    /**
+     * 查询学生列表
+     */
+    @PreAuthorize("@ss.hasPermi('test:institute:list')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
+    @GetMapping("/list")
+    public TableDataInfo list(Student student)
+    {
+        startPage();
+        List<Student> list = studentService.selectAllStudents(student);
+        return getDataTable(list);
     }
+
+    /*
+     *  ID查询学生
+     * */
+    /*@PreAuthorize("@ss.hasRole('boss')")
+    @GetMapping(value = "/{no}")
+    public AjaxResult getInfo(@PathVariable("no") Integer no)
+    {
+        return AjaxResult.success(studentMapper.selectStudentByUserId(no));
+    }*/
 
 
 
     /**
      * 获取test详细信息
      */
-//    @PreAuthorize("@ss.hasPermi('test:test:query')")
-    @PreAuthorize("@ss.hasRole('boss')")
+    /*@PreAuthorize("@ss.hasPermi('test:class:query')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @GetMapping(value = "/{userId}")
     public AjaxResult getInfo(@PathVariable("userId") Long userId)
     {
         return AjaxResult.success(testUserService.selectTestUserByUserId(userId));
+    }*/
+
+    @PreAuthorize("@ss.hasPermi('test:institute:query')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
+    @GetMapping(value = "/{no}")
+    public AjaxResult getInfo(@PathVariable("no") Integer no)
+    {
+        return AjaxResult.success(studentService.selectTestUserByUserId(no));
     }
+
+
+
+
 
     /**
      * 修改test
      */
-//    @PreAuthorize("@ss.hasPermi('test:test:edit')")
-    @PreAuthorize("@ss.hasRole('boss')")
+    @PreAuthorize("@ss.hasPermi('test:institute:edit')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @Log(title = "test", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody TestUser testUser)
@@ -67,12 +112,56 @@ public class TestUserInstituteController extends BaseController
     /**
      * 删除test
      */
-//    @PreAuthorize("@ss.hasPermi('test:test:remove')")
-    @PreAuthorize("@ss.hasRole('boss')")
+    @PreAuthorize("@ss.hasPermi('test:institute:remove')")
+//    @PreAuthorize("@ss.hasRole('instructor','boss','monitor')")
     @Log(title = "test", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{userIds}")
+    @DeleteMapping("/{userIds}")
     public AjaxResult remove(@PathVariable Long[] userIds)
     {
         return toAjax(testUserService.deleteTestUserByUserIds(userIds));
     }
+
+
+    /**
+     * 驳回学生申请
+     */
+    @PreAuthorize("@ss.hasPermi('test:institute:back')")
+    @Log(title = "back", businessType = BusinessType.UPDATE)
+    @PutMapping("backUser")
+    /*public AjaxResult backTestUser(@RequestBody TestUser testUser)
+    {
+        return toAjax(testUserService.backTestUser(testUser));
+    }*/
+    public AjaxResult backTestUser(@RequestBody Testersoure testersoure){
+        Student student = new Student();
+        student.setStatus("2");
+        student.setNo(testersoure.getNo());
+        studentService.updateStatusByNo(student);
+        return toAjax(testersoureService.updateReasonByNo(testersoure));
+    }
+
+    /**
+     * 批量通过学生申请
+     */
+    @PreAuthorize("@ss.hasPermi('test:institute:access')")
+    @Log(title = "access", businessType = BusinessType.UPDATE)
+    @PutMapping("/{nos}")
+    public AjaxResult accessUser(@PathVariable String nos){
+        return toAjax(studentService.updateStatusByNos(nos));
+    }
+
+    /**
+     * 通过单个学生
+     */
+    @PreAuthorize("@ss.hasPermi('test:institute:access2')")
+    @Log(title = "access2", businessType = BusinessType.UPDATE)
+    @PutMapping("accessUser2")
+    public AjaxResult accessUser2(@RequestBody Student student){
+        student.setStatus("1");
+        return toAjax(studentService.updateStatusByNo(student));
+    }
+
+
+
 }
+
