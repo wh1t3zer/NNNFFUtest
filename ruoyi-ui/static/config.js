b65404943d62b1aa3d826b5b6776f448033159b8
山有木兮木有枝 <8227036+a602468057@user.noreply.gitee.com>
2022-08-13 23:50:40 +0800

    'config配置文件'

--- ruoyi-ui/static/config.js
+++ ruoyi-ui/static/config.js
@@ -1,35 +1,407 @@
-export const majorOptionsini= {
-    majorOptions: [
-        {
-          majorValue: '计算机科学与技术',
-          label: '计算机科学与技术'
-        }, {
-          majorValue: '数据科学与大数据技术',
-          label: '数据科学与大数据技术'
-        },{
-          majorValue: '电子信息科学与技术',
-          label: '电子信息科学与技术'
-        },{
-          majorValue: '电气工程及其自动化',
-          label: '电气工程及其自动化'
-        },{
-          majorValue: '软件工程',
-          label: '软件工程'
-        },{
-          majorValue: '智能科学与技术',
-          label: '智能科学与技术'
-        }, {
-          majorValue: '通信工程',
-          label: '通信工程'
-        }],
+export const majorOptionsini = {
+  majorOptions: [
+    {
+      majorValue: '计算机科学与技术',
+      label: '计算机科学与技术'
+    }, {
+      majorValue: '数据科学与大数据技术',
+      label: '数据科学与大数据技术'
+    }, {
+      majorValue: '电子信息科学与技术',
+      label: '电子信息科学与技术'
+    }, {
+      majorValue: '电气工程及其自动化',
+      label: '电气工程及其自动化'
+    }, {
+      majorValue: '软件工程',
+      label: '软件工程'
+    }, {
+      majorValue: '智能科学与技术',
+      label: '智能科学与技术'
+    }, {
+      majorValue: '通信工程',
+      label: '通信工程'
+    }],
 }
-export const classOptionsini= {
+export const classOptionsini = {
   classOptions: [
-      {
-        classValue: '20软件1班',
-        label: '20软件1班'
-      }, {
-        classValue: '20软件2班',
-        label: '20软件2班'
-      }],
+    {
+      classValue: '20软件1班',
+      label: '20软件1班'
+    }, {
+      classValue: '20软件2班',
+      label: '20软件2班'
+    },
+    {
+      classValue: '20软件3班',
+      label: '20软件3班'
+    },
+    {
+      classValue: '20软件4班',
+      label: '20软件4班'
+    },
+    {
+      classValue: '20软件5班',
+      label: '20软件5班'
+    },
+    {
+      classValue: '20软件6班',
+      label: '20软件6班'
+    },
+    {
+      classValue: '20软件7班',
+      label: '20软件7班'
+    },
+    {
+      classValue: '20计算机1班',
+      label: '20计算机1班'
+    }, {
+      classValue: '20计算机2班',
+      label: '20计算机2班'
+    },
+    {
+      classValue: '20计算机3班',
+      label: '20计算机3班'
+    },
+    {
+      classValue: '20计算机4班',
+      label: '20计算机4班'
+    },
+    {
+      classValue: '20计算机5班',
+      label: '20计算机5班'
+    },
+    {
+      classValue: '20计算机6班',
+      label: '20计算机6班'
+    },
+    {
+      classValue: '20计算机7班',
+      label: '20计算机7班'
+    },
+    {
+      classValue: '20计算机中外1班',
+      label: '20计算机中外1班'
+    },
+    {
+      classValue: '20达人计算机班',
+      label: '20达人计算机班'
+    },
+
+    {
+      classValue: '20数据科学与大数据技术1班',
+      label: '20数据科学与大数据技术1班'
+    },
+    {
+      classValue: '20数据科学与大数据技术2班',
+      label: '20数据科学与大数据技术2班'
+    },
+    {
+      classValue: '20数据科学与大数据技术3班',
+      label: '20数据科学与大数据技术3班'
+    },
+    {
+      classValue: '20电子1班',
+      label: '20电子1班'
+    },
+    {
+      classValue: '20电子2班',
+      label: '20电子2'
+    },
+    {
+      classValue: '20电子3班',
+      label: '20电子3班'
+    },
+    {
+      classValue: '20电子4班',
+      label: '20电子4班'
+    },
+    {
+      classValue: '20电子商务1班',
+      label: '20电子商务1班'
+    },
+    {
+      classValue: '20电子商务2班',
+      label: '20电子商务2'
+    },
+    {
+      classValue: '20电子商务3班',
+      label: '20电子商务3班'
+    },
+    {
+      classValue: '20电子商务4班',
+      label: '20电子商务4班'
+    },
+    {
+      classValue: '20电气1班',
+      label: '20电气1班'
+    },
+    {
+      classValue: '20电气2班',
+      label: '20电气2'
+    },
+    {
+      classValue: '20电气3班',
+      label: '20电气3班'
+    },
+    {
+      classValue: '20电气4班',
+      label: '20电气4班'
+    },
+    {
+      classValue: '20电气5班',
+      label: '20电气5班'
+    },
+    {
+      classValue: '20智能科学与技术1班',
+      label: '20智能科学与技术1班'
+    },
+    {
+      classValue: '20智能科学与技术2班',
+      label: '20智能科学与技术2'
+    },
+    {
+      classValue: '20智能科学与技术3班',
+      label: '20智能科学与技术3班'
+    },
+    {
+      classValue: '20通信1班',
+      label: '20通信1班'
+    }, {
+      classValue: '20通信2班',
+      label: '20通信2班'
+    },
+    {
+      classValue: '20通信3班',
+      label: '20通信3班'
+    },
+    {
+      classValue: '20通信4班',
+      label: '20通信4班'
+    },
+
+
+    {
+      classValue: '19软工1班',
+      label: '19软工1班'
+    },
+    {
+      classValue: '19软工2班',
+      label: '19软工2班'
+    },
+    {
+      classValue: '19软工3班',
+      label: '19软工3班'
+    },
+    {
+      classValue: '19软工4班',
+      label: '19软工4班'
+    },
+    {
+      classValue: '19软工5班',
+      label: '19软工5班'
+    },
+    {
+      classValue: '19软工6班',
+      label: '19软工6班'
+    },
+    {
+      classValue: '19软工7班',
+      label: '19软工7班'
+    },
+    {
+      classValue: '19软工8班',
+      label: '19软工8班'
+    },
+    {
+      classValue: '19软工9班',
+      label: '19软工9班'
+    },
+    {
+      classValue: '19软工10班',
+      label: '19软工10班'
+    },
+    {
+      classValue: '19软工11班',
+      label: '19软工11班'
+    },
+    {
+      classValue: '19软工12班',
+      label: '19软工12班'
+    },
+    {
+      classValue: '19软工13班',
+      label: '19软工13班'
+    },
+    {
+      classValue: '19计算机1班',
+      label: '19计算机1班'
+    },
+    {
+      classValue: '19计算机2班',
+      label: '19计算机2班'
+    },
+    {
+      classValue: '19计算机3班',
+      label: '19计算机3班'
+    },
+    {
+      classValue: '19计算机4班',
+      label: '19计算机4班'
+    },
+    {
+      classValue: '19计算机5班',
+      label: '19计算机5班'
+    },
+    {
+      classValue: '19计算机6班',
+      label: '19计算机6班'
+    },
+    {
+      classValue: '19计算机7班',
+      label: '19计算机7班'
+    },
+    {
+      classValue: '19计算机8班',
+      label: '19计算机8班'
+    },
+    {
+      classValue: '19计算机9班',
+      label: '19计算机9班'
+    },
+    {
+      classValue: '19计算机10班',
+      label: '19计算机10班'
+    },
+    {
+      classValue: '19计算机11班',
+      label: '19计算机11班'
+    },
+    {
+      classValue: '19计算机12班',
+      label: '19计算机12班'
+    },
+    {
+      classValue: '19计算机13班',
+      label: '19计算机13班'
+    },
+    {
+      classValue: '19达人计算机班',
+      label: '19达人计算机班'
+    },
+
+    {
+      classValue: '19数技1班',
+      label: '19数技1班'
+    },
+    {
+      classValue: '19数技2班',
+      label: '19数技2班'
+    },
+    {
+      classValue: '19数技3班',
+      label: '19数技3班'
+    },
+    {
+      classValue: '19数技4班',
+      label: '19数技4班'
+    },
+
+
+    {
+      classValue: '19电子1班',
+      label: '19电子1班'
+    },
+    {
+      classValue: '19电子2班',
+      label: '19电子2班'
+    },
+    {
+      classValue: '19电子3班',
+      label: '19电子3班'
+    },
+    {
+      classValue: '19电子4班',
+      label: '19电子4班'
+    },
+    {
+      classValue: '19电子5班',
+      label: '19电子5班'
+    },
+    {
+      classValue: '19电气1班',
+      label: '19电气1班'
+    },
+    {
+      classValue: '19电气2班',
+      label: '19电气2班'
+    },
+    {
+      classValue: '19电气3班',
+      label: '19电气3班'
+    },
+    {
+      classValue: '19电气4班',
+      label: '19电气4班'
+    },
+    {
+      classValue: '19电气5班',
+      label: '19电气5班'
+    },
+    {
+      classValue: '19电气（专升本）1班',
+      label: '19电气（专升本）1班'
+    },
+
+
+    {
+      classValue: '19智能科学1班',
+      label: '19智能科学1班'
+    },
+    {
+      classValue: '19智能科学2班',
+      label: '19智能科学2班'
+    },
+    {
+      classValue: '19智能科学3班',
+      label: '19智能科学3班'
+    },
+    {
+      classValue: '19智能科学4班',
+      label: '19智能科学4班'
+    },
+    {
+      classValue: '19智能科学5班',
+      label: '19智能科学5班'
+    },
+    {
+      classValue: '19智能科学6班',
+      label: '19智能科学6班'
+    },
+
+
+    {
+      classValue: '19通信1班',
+      label: '19通信1班'
+    },
+    {
+      classValue: '19通信2班',
+      label: '19通信2班'
+    },
+    {
+      classValue: '19通信3班',
+      label: '19通信3班'
+    },
+    {
+      classValue: '19通信4班',
+      label: '19通信4班'
+    },
+    {
+      classValue: '19通信5班',
+      label: '19通信5班'
+    },
+
+
+
+  ],
 }
